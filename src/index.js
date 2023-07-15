028afb10e19695db32f03fb4ee061d1bd48a667d
Peder Goodman <peder.goodman@gmail.com>
2023-07-14 20:08:13 -0500

    imported redux and added reducers to index.js

--- src/index.js
+++ src/index.js
@@ -3,9 +3,62 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './components/App/App';
 
+// Redux imports
+import { Provider } from 'react-redux';
+import { createStore, combineReducers, applyMiddleware } from 'redux';
+import logger from 'redux-logger'
+
+// REDUCERS
+// feeling reducer
+const feeling = (state = 0, action) => {
+    if (action.type === 'ADD_FEELING') {
+        return state = action.payload
+    } else if (action.type === 'ADD_FEELING') {
+        return state = 0
+    }
+};
+
+// understanding reducer
+const understanding = (state = 0, action) => {
+    if (action.type === 'ADD_UNDERSTANDING') {
+        return state = action.payload
+    } else if (action.type === 'ADD_UNDERSTANDING') {
+        return state = 0
+    }
+};
+
+// support reducer
+const support = (state = 0, action) => {
+    if (action.type === 'ADD_SUPPORT') {
+        return state = action.payload
+    } else if (action.type === 'ADD_SUPPORT') {
+        return state = 0
+    }
+};
+
+// comment reducer
+const comment = (state = '', action) => {
+    if (action.type === 'ADD_COMMENT') {
+        return state = action.payload
+    } else if (action.type === 'ADD_COMMENT') {
+        return state = ''
+    }
+};
+
+
+// STORE
+const store = createStore(
+    combineReducers({
+       feeling, understanding, support, comment 
+    }), applyMiddleware(logger)
+);
+
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-    <React.StrictMode>
-        <App />
-    </React.StrictMode>
+    <Provider store={store} >
+        <React.StrictMode>
+            <App />
+        </React.StrictMode>
+    </Provider>
 );
