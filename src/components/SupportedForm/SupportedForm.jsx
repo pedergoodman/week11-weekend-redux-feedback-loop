98d8273c281d3ef55373ba776239ffefd5510182
Peder Goodman <peder.goodman@gmail.com>
2023-07-15 11:54:24 -0500

    copied form code to supported page, changed values

--- src/components/SupportedForm/SupportedForm.jsx
+++ src/components/SupportedForm/SupportedForm.jsx
@@ -1,7 +1,108 @@
-import React from 'react'
+import React, { useEffect, useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
 
+import { useHistory } from "react-router-dom/";
+
+// Material UI imports
+import Box from "@mui/material/Box";
+import TextField from "@mui/material/TextField";
+import ArrowForwardIcon from "@mui/icons-material/ArrowForward";
+import ArrowBackIcon from "@mui/icons-material/ArrowBack";
+import { Button, IconButton, Typography } from "@mui/material";
+import MenuItem from "@mui/material/MenuItem";
+
+// start component Function
 export default function SupportedForm() {
+  // options for input form
+  const ratings = [1, 2, 3, 4, 5];
+  const storeSupportedValue = useSelector(store => store.survey.supported);
+  const dispatch = useDispatch();
+  const history = useHistory()
+
+  // local state to track input selection
+  const [supportedValue, setSupportedValue] = useState("");
+  // form validation state
+  const [isEmpty, setIsEmpty] = useState(false);
+
+  // update selected value, handle form validation condition
+  const handleChange = event => {
+    setSupportedValue(event.target.value);
+    setIsEmpty(false);
+  };
+
+  // useEffect to load saved state from store
+  useEffect(() => {
+    setSupportedValue(storeSupportedValue);
+  }, []);
+
+  // handleClickNext function
+  const handleClickNext = event => {
+    console.log("next clicked! Value is:", supportedValue);
+    // validate form input
+    if (!supportedValue) {
+      setIsEmpty(true);
+    } else {
+      // Dispatch to reducer
+      dispatch({
+        type: 'SUPPORTED',
+        payload: supportedValue
+      })
+      // move to next page
+      history.push('/comment')
+
+    }
+    console.log("isEmpty is", isEmpty);
+  };
+
+  // TODO - handleClickBack function
+
   return (
-    <div>SupportedForm</div>
-  )
+    <>
+      <Box
+        component="form"
+        sx={{
+          textAlign: "center",
+          width: "fit-content",
+          margin: "auto",
+        }}
+        noValidate
+        autoComplete="off"
+      >
+        <h3>How well are you being supported?</h3>
+        <div>
+          <TextField
+            required
+            id="feeling-input"
+            select
+            label="Select"
+            defaultValue=""
+            value={supportedValue}
+            onChange={handleChange}
+            sx={{ textAlign: "center", width: 175 }}
+            helperText={
+              isEmpty ? "please submit a value." : "please add a value"
+            }
+            error={isEmpty}
+          >
+            {ratings.map(option => (
+              <MenuItem key={option} value={option}>
+                {option}
+              </MenuItem>
+            ))}
+          </TextField>
+        </div>
+        <div className="form-btn-container">
+          <IconButton aria-label="next" disabled>
+            <ArrowBackIcon />
+          </IconButton>
+
+          {/* <Button size="large">Home</Button> */}
+
+          <IconButton aria-label="next" onClick={handleClickNext}>
+            <ArrowForwardIcon />
+          </IconButton>
+        </div>
+      </Box>
+    </>
+  );
 }
