6a7b77f776d0dd0b538bf6206c3320f1414420d6
Peder Goodman <peder.goodman@gmail.com>
2023-07-15 19:05:11 -0500

    added home page

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,7 +1,47 @@
-import React from 'react'
+import React from "react";
+import { useDispatch } from "react-redux";
 
-export default function Home() {
+import { useHistory } from "react-router-dom/";
+
+// Material UI imports
+import Box from "@mui/material/Box";
+import { Button, IconButton, Typography } from "@mui/material";
+
+export default function Submitted() {
+  const history = useHistory();
+  const dispatch = useDispatch();
+
+  const handleClickHome = () => {
+    // clear imputs and move to home page
+    dispatch({ type: "CLEAR" });
+    history.push("/");
+  };
+
+  const handleClickNewFeedback = () => {
+    // clear imputs and move to home page
+
+    dispatch({ type: "CLEAR" });
+    history.push("/feeling");
+  };
+
   return (
-    <div>Home</div>
-  )
+    <>
+      <Box id='home-container' className="form-container" noValidate autoComplete="off">
+        <h2>Please add your Feedback!</h2>
+
+        <div className="home-btn-container">
+          {/* <Button size="large">Home</Button> */}
+
+          <Button
+            sx={{width: 130, height: 50, fontSize: '1.3rem', fontWeight: 200}}
+            aria-label="submit"
+            variant="contained"
+            onClick={handleClickNewFeedback}
+          >
+            Start
+          </Button>
+        </div>
+      </Box>
+    </>
+  );
 }
